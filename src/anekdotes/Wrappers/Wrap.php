620a2efdb7f7e284833b75e8ee13968d4cc669ff
Grasseh <steve.gagnev4si@gmail.com>
2016-07-21 14:38:44 -0400

    Added Wrap Wrapper

---
+++ src/anekdotes/Wrappers/Wrap.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the Logger package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes\FormWrapper\Wrappers;
+
+/**
+ * Doesn't wrap the input in anything.
+ */
+class Wrap
+{
+    public function handle($title, $input)
+    {
+        return  '<div class="row form-row">'.
+                    '<label class="col col-lg-4 form-label">'.$title.'</label>'.
+                    '<div class="col col-lg-8">'.
+                        $input.
+                    '</div>'.
+                    '<div class="clearfix"></div>'.
+                '</div>'."\n";
+    }
+}
