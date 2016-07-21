532089543dfb98c70e1be0672711a5b64a6b87df
Grasseh <steve.gagnev4si@gmail.com>
2016-07-21 14:08:03 -0400

    Added Above wrapper

---
+++ src/anekdotes/Wrappers/Above.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the Logger package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes\FormWrapper\Wrappers;
+
+/**
+ * Doesn't wrap the input in anything.
+ */
+class Above
+{
+    public function handle($title, $input)
+    {
+        return  '<div class="row form-row">'.
+                    '<div class="col-lg-12"><label>'.$title.'</label></div>'.
+                    '<div class="col-lg-12">'.
+                        $input.
+                    '</div>'.
+                    '<div class="clearfix"></div>'.
+                '</div>'."\n";
+    }
+}
