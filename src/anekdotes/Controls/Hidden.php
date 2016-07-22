41380393b2b669075e620de2195f79aade71c99d
Grasseh <steve.gagnev4si@gmail.com>
2016-07-22 09:44:39 -0400

    Added Hidden Control

---
+++ src/anekdotes/Controls/Hidden.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of the Logger package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes\FormWrapper\Controls;
+
+/**
+ * Wraps a hidden field.
+ */
+class Hidden extends Control
+{
+    protected $nbParams = 3;
+
+    /**
+     * Prepare the input.
+     *
+     * @param array $arguments Contains the passed arguments
+     *                         [0]  string    $name   Field's name, to be used by the form as a key.
+     *                         [1]  string[]  $opts   Contains the option="value" key-value pairs to be added to the field
+     *                         [2]  string    $value  Contains the field's default value.
+     */
+    public function prepare($arguments)
+    {
+        $name = $arguments[0];
+        $opts = $arguments[1];
+        $value = $arguments[2];
+        $html = '<input type="hidden" name="'.$name.'" value="'.$value.'"';
+        $html .= $this->getOpts($opts);
+        $html .= '/>';
+
+        return $html;
+    }
+}
