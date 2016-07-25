1ea1a2f965236120777ca69b39be92cd2dc4a0e6
Grasseh <steve.gagnev4si@gmail.com>
2016-07-25 09:50:07 -0400

    Added Tests + basic css wooo

---
+++ examples/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/../src/anekdotes/assets/min/Form.css">
+</head>
+<body>
+    <h1> Basic Test </h1>
+<?php
+    require __DIR__ . '/../vendor/autoload.php';
+    $form = new \Anekdotes\FormWrapper\Form();
+    echo($form->open('http://www.github.com', "post", ["class" => "form"]));
+    echo($form->Text('Name', 'name', ["class" => "form-stuff"], 'Form Wrapper', 'Wrap'));
+    echo($form->Submit('Go To Github', 'Click Here', ["class" => "btn"], 'Wrap'));
+    echo($form->close());
+?>
+</body>
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// =======================================================================================
+// Necessary stuff
+// =======================================================================================
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+// =======================================================================================
+// =======================================================================================
+
+// =======================================================================================
+// Tasks
+// =======================================================================================
+//default task => gulp
+gulp.task('default', [
+  'css',
+]);
+
+//watch task => gulp watch
+gulp.task('watch', function() {
+  gulp.watch([
+    './src/anekdotes/assets/Form.scss'
+  ], ['css']);
+});
+
+// css task => gulp ccss
+gulp.task('css', function() {
+  return gulp.src([
+    'src/anekdotes/assets/Form.scss',
+    'src/anekdotes/assets/bootstrap.css'
+  ])
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(concat('Form.css'))
+  .pipe(minifyCSS({
+    keepBreaks:true
+  }))
+  .pipe(gulp.dest('./src/anekdotes/assets/min/'));
+});
+// =======================================================================================
+// =======================================================================================
+
