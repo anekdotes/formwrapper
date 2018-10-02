94f8ee4e95878075b5243875a693de04a076d313
sbolduc <samuel.bolduc@anekdotes.com>
2018-10-02 15:44:39 -0400

    new LTE wrap

---
+++ src/anekdotes/Wrappers/LteWrap.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Form Wrapper package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes\FormWrapper\Wrappers;
+
+/**
+ * Doesn't wrap the input in anything.
+ */
+class newWrap
+{
+    public function handle($input)
+    {
+        return  '<div class="form-group">'.
+                    '<label>'.$title.'</label>'.
+                    $input.
+                '</div>'."\n";
+    }
+}
